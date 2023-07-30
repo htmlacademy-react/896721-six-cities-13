c26531ba53bbe0f0ea0467a902382e933bae9014
relecteve <Filonovaval@gmail.com>
2023-07-30 20:28:31 +0300

    Добавление маршрутов

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,10 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const.ts';
 import MainHotelCards from '../../pages/mainHotelCards/mainHotelCards';
+import Favorites from '../../pages/favorites/favorites.tsx';
+import Login from '../../pages/login/login.tsx';
+import OfferHotel from '../../pages/offer/offer.tsx';
+import Message404 from '../../pages/404/404.tsx';
 
 type AppScreenProps = {
   cardHotelCount: number;
@@ -6,7 +12,30 @@ type AppScreenProps = {
 
 function App({cardHotelCount}: AppScreenProps): JSX.Element {
   return (
-    <MainHotelCards cardHotelCount={cardHotelCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainHotelCards cardHotelCount={cardHotelCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<Favorites />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferHotel />}
+        />
+        <Route
+          path="*"
+          element={<Message404 />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
